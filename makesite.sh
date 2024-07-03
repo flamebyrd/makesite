e63308722d09045d1e018142b1764973c040c438
Flamebyrd <flamebyrd@gmail.com>
2024-07-03 15:40:45 +0800

    Adding sample works

--- makesite.sh
+++ makesite.sh
@@ -6,11 +6,11 @@ if [ -d "$VENV" ]; then
     # Activate the existing virtual environment
     source venv/bin/activate
 else
-    python3 -m venv venv
+    python3 -m venv venv || python -m venv venv
     source venv/bin/activate
-    python3 -m pip install --upgrade pip
-    pip3 install -r requirements.txt
+    python3 -m pip install --upgrade pip || python3 -m pip install --upgrade pip
+    pip3 install -r requirements.txt || pip install -r requirements.txt
 fi
-python3 makesite.py
-python3 -m http.server -d _site 
+python3 makesite.py || python makesite.py
+python3 -m http.server -d _site || python -m http.server -d _site
 
